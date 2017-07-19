351ff8c1bdf9a9b166cf3c6001599fde629b4242
Ben Shoemaker <bshoemaker@setonhill.edu>
2017-07-18 20:57:39 -0400

    Lets see if we can get the balance.

--- src/controllers.php
+++ src/controllers.php
@@ -9,6 +9,9 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function () use ($app) {
+    $chainCoinInfo = array();
+    exec('chaincoind getinfo', $chainCoinInfo);
+    print_r($chainCoinInfo);
     return $app['twig']->render('index.html.twig', array());
 })
 ->bind('homepage');
