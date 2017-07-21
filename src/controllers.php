f757667857c5a5208f2b9e65e218a3e86038a986
Ben Shoemaker <bshoemaker@setonhill.edu>
2017-07-20 21:54:14 -0400

    SCRILLA!

--- src/controllers.php
+++ src/controllers.php
@@ -23,7 +23,7 @@ $app->get('/transfer', function () use ($app) {
     $nodeInfo = $app['chaincoin']->getInfo();
     $balance = $nodeInfo['balance'];
 
-    return $app['twig']->render('transfer.html.twig', array('balance'=> $balance, 'errorMsg' => $errorMsg));
+    return $app['twig']->render('transfer.html.twig', array('balance'=> 120105.05282001, 'errorMsg' => $errorMsg));
 })
 ->bind('transfer_coins');
 
